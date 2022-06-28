f05a3ca02466aac97a4f6672fe6870a398d01cce
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-28 18:30:05 +0200

    Updates

--- source/layouts/tabler/components/forms/imagechecks/package.d
+++ source/layouts/tabler/components/forms/imagechecks/package.d
@@ -6,5 +6,6 @@ import layouts.tabler;
 public import layouts.tabler.components.forms.imagechecks.figure;
 public import layouts.tabler.components.forms.imagechecks.input;
 public import layouts.tabler.components.forms.imagechecks.image;
+public import layouts.tabler.components.forms.imagechecks.imagecontent;
 public import layouts.tabler.components.forms.imagechecks.label;
 
