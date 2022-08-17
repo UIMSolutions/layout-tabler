ab8803eb53f1bb4c8cd83d23d7d457498bd276e1
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-17 07:33:33 +0200

    Update header.d

--- source/layouts/tabler/components/empties/header.d
+++ source/layouts/tabler/components/empties/header.d
@@ -9,5 +9,5 @@ class DBS5EmptyHeader : DBS5Obj {
 mixin(H5Calls!"BS5EmptyHeader");
 
 version(test_layout_tabler) { unittest {
-	assert(BS5EmptyHeader, `<div class="empty-header"></div>`);
+	assert(BS5EmptyHeader == `<div class="empty-header"></div>`);
 }}
