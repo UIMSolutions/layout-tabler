52be33cd19a59028bb329e5aa5b0e522bb9bf197
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-17 07:28:32 +0200

    Update empty.d

--- source/layouts/tabler/components/empties/empty.d
+++ source/layouts/tabler/components/empties/empty.d
@@ -1,4 +1,4 @@
-﻿module layouts.tabler.components.empties.empty;
+module layouts.tabler.components.empties.empty;
 
 @safe:
 import layouts.tabler;
@@ -7,28 +7,20 @@ class DBS5Empty : DBS5Obj {
 	mixin(H5This!("div", ["empty"]));
 
 	mixin(MyContent!("action", "BS5EmptyAction"));
-	unittest {}
 
 	mixin(MyContent!("icon", "BS5EmptyIcon"));
-	unittest {}
 
 	mixin(MyContent!("image", "BS5EmptyImage"));
-	unittest {
-		assert(BS5Empty.image, `<div class="empty"><div class="empty-img"></div></div>`);
-	}
 
 	mixin(MyContent!("header", "BS5EmptyHeader"));
-	unittest {}
 
 	mixin(MyContent!("subtitle", "BS5EmptySubtitle"));
-	unittest {}
 
 	mixin(MyContent!("title", "BS5EmptyTitle"));
-	unittest {}
-
 }
 mixin(H5Calls!"BS5Empty");
 
-unittest {
+version(test_layout_tabler) { unittest {
 	assert(BS5Empty, `<div class="empty"></div>`);
-}
+		assert(BS5Empty.image, `<div class="empty"><div class="empty-img"></div></div>`);
+}}
