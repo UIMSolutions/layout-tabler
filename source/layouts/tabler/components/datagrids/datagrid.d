550fe4529e68eeab5597224cfc1fcac3a6886d09
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-17 07:18:53 +0200

    Update datagrid.d

--- source/layouts/tabler/components/datagrids/datagrid.d
+++ source/layouts/tabler/components/datagrids/datagrid.d
@@ -7,12 +7,10 @@ class DBS5Datagrid : DBS5Obj {
 	mixin(H5This!("div", ["datagrid"]));
 
   O color(this O)(string value){ this.classes("bg-"~value); return cast(O)this; }
-	unittest {
-		assert(BS5Datagrid.color("blue"),`<div class="bg-blue datagrid"></div>`);
-	}
 }
 mixin(H5Calls!"BS5Datagrid");
 
-unittest {
+version(test_layout_tabler) { unittest {
 	assert(BS5Datagrid, `<div class="datagrid"></div>`);
-}
+	assert(BS5Datagrid.color("blue"),`<div class="bg-blue datagrid"></div>`);
+}}
