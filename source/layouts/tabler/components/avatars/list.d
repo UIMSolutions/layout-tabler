70f9791a9802a3cb547a3c4fbe6cfa9e053011e0
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-17 07:26:12 +0200

    Update list.d

--- source/layouts/tabler/components/avatars/list.d
+++ source/layouts/tabler/components/avatars/list.d
@@ -1,4 +1,4 @@
-﻿module layouts.tabler.components.avatars.list;
+module layouts.tabler.components.avatars.list;
 
 @safe:
 import layouts.tabler;
@@ -7,13 +7,10 @@ class DBS5AvatarList : DBS5Obj {
 	mixin(H5This!("div", ["avatar-list"]));
 
 	mixin(MyContent!("avatar", "BS5Avatar"));
-	unittest {
-		assert(BS5AvatarList.avatar, `<div class="avatar-list"><span class="avatar"></span></div>`);
-	}
-
 }
 mixin(H5Calls!"BS5AvatarList");
 
-unittest {
+version(test_layout_tabler) { unittest {
 	assert(BS5AvatarList, `<div class="avatar-list"></div>`);
-}
+	assert(BS5AvatarList.avatar, `<div class="avatar-list"><span class="avatar"></span></div>`);
+}}
