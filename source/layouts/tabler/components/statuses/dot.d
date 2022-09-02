230e50a07a3c9c06678c22dc7e575898cec835a2
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-09-02 09:49:45 +0200

    Update

--- source/layouts/tabler/components/statuses/dot.d
+++ source/layouts/tabler/components/statuses/dot.d
@@ -7,17 +7,13 @@ class DBS5StatusDot : DBS5Obj {
 	mixin(H5This!("span", ["status-dot"]));
 
   O animated(this O)(bool value = true){ if (value) this.classes("status-dot-animated"); return cast(O)this; }
-	version(test_layout_tabler) { unittest {
-		assert(BS5StatusDot.animated,`<span class="status-dot status-dot-animated"></span>`);
-	}}
 
   O color(this O)(string value){ this.classes("status-"~value); return cast(O)this; }
-	version(test_layout_tabler) { unittest {
-		assert(BS5Status.color("blue"),`<span class="status-dot status-blue"></span>`);
-	}}
 }
 mixin(H5Calls!"BS5StatusDot");
 
 version(test_layout_tabler) { unittest {
 	assert(BS5StatusDot, `<span class="status-dot"></span>`);
+  assert(BS5StatusDot.animated,`<span class="status-dot status-dot-animated"></span>`);
+  assert(BS5Status.color("blue"),`<span class="status-dot status-blue"></span>`);
 }}
--- source/layouts/tabler/components/timelines/icon.d
+++ source/layouts/tabler/components/timelines/icon.d
@@ -7,12 +7,10 @@ class DBS5TimelineIcon : DBS5Obj {
 	mixin(H5This!("div", ["list-timeline-icon"]));
 
   O color(this O)(string value){ this.classes("bg-"~value); return cast(O)this; }
-	version(test_layout_tabler) { unittest {
-		assert(BS5TimelineIcon.color("blue"),`<div class="bg-blue list-timeline-icon"></div>`);
-	}}
 }
 mixin(H5Calls!"BS5TimelineIcon");
 
 version(test_layout_tabler) { unittest {
 	assert(BS5TimelineIcon, `<span class="status"></span>`);
+	assert(BS5TimelineIcon.color("blue"),`<div class="bg-blue list-timeline-icon"></div>`);
 }}
